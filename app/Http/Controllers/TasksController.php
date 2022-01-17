c1073d0e69efc2638d3110066edad939f5f1021f
yuusuke.oose <uiternia0910@gmail.com>
2022-01-17 12:22:26 +0000

    kadai-tasklist

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class TasksController extends Controller
+
+{
+    // getでmessages/にアクセスされた場合の「一覧表示処理」
+    public function index()
+    {
+        // メッセージ一覧を取得
+        $tasks = task::all();
+
+        // メッセージ一覧ビューでそれを表示
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    // getでmessages/createにアクセスされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        $task = new Task;
+        
+        //メッセージ作成ビューを表示
+        return view('tasks.create',[
+            'task' => $task,
+            ]);
+    }
+
+    // postでmessages/にアクセスされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+         // メッセージを作成
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    // getでmessages/（任意のid）にアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+         // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // メッセージ詳細ビューでそれを表示
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    // getでmessages/（任意のid）/editにアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // メッセージ編集ビューでそれを表示
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    // putまたはpatchでmessages/（任意のid）にアクセスされた場合の「更新処理」
+    public function update(Request $request, $id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを更新
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    // deleteでmessages/（任意のid）にアクセスされた場合の「削除処理」
+    public function destroy($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを削除
+        $task->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+}
