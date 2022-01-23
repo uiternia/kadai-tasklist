97109805d37eaf27b19d0cbae28dc2d22041a342
yuusuke.oose <uiternia0910@gmail.com>
2022-01-23 06:44:09 +0000

    tasklist

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
     @else
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to the Microposts</h1>
+                <h1>Welcome to the Tasklist</h1>
                 {{-- ユーザ登録ページへのリンク --}}
                 {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
